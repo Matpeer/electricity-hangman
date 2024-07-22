ce7a3134f9b4cf633982e76ee8f4b8e879c944a3
Matpeer <matpeer@gmail.com>
2024-07-22 11:50:00 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 ```javascript
-console.log("app.js loaded");
-
-function SimpleComponent() {
-    return React.createElement('div', null, 'Hello from React!');
-}
-
-console.log("About to render SimpleComponent");
-ReactDOM.render(React.createElement(SimpleComponent), document.getElementById('root'));
-console.log("Finished rendering SimpleComponent");
+document.addEventListener('DOMContentLoaded', function() {
+    var app = document.getElementById('app');
+    app.innerHTML = 'המשחק נטען בהצלחה!';
+});
 ```
