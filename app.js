db82b761748565b123a3b10ace47a9d26ef0f75a
Matpeer <matpeer@gmail.com>
2024-07-22 11:41:46 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 ```javascript
 console.log("app.js loaded");
 
-const electricityWords = [
-    { word: 'מטען', hint: 'תכונה בסיסית של חלקיקים המשפיעה על כוחות חשמליים', definition: 'כמות החשמל שיש לגוף או לחלקיק' },
-    { word: 'זרם', hint: 'תנועה מסודרת של מטענים חשמליים', definition: 'תנועה של מטענים חשמליים דרך מוליך' },
-    { word: 'מתח', hint: 'ההפרש בפוטנציאל החשמלי בין שתי נקודות', definition: 'מידת הכוח שדוחף את האלקטרונים במעגל חשמלי' },
-    { word: 'התנגדות', hint: 'מידת ההתנגדות של חומר לזרימת חשמל', definition: 'היכולת של חומר להתנגד לזרימת זרם חשמלי' },
-    { word: 'קבל', hint: 'רכיב חשמלי המאחסן מטען חשמלי', definition: 'מכשיר המאחסן אנרגיה חשמלית בשדה חשמלי' }
-];
-
-function HangmanGame() {
-    console.log("HangmanGame component rendered");
-    // ... rest of the component code ...
+function SimpleComponent() {
+    return React.createElement('div', null, 'Hello from React!');
 }
 
-console.log("About to render HangmanGame");
-ReactDOM.render(<HangmanGame />, document.getElementById('root'));
-console.log("Finished rendering HangmanGame");
+console.log("About to render SimpleComponent");
+ReactDOM.render(React.createElement(SimpleComponent), document.getElementById('root'));
+console.log("Finished rendering SimpleComponent");
 ```
